3522408d77a7dfd6570fe3c7a6ecf773072c329c
soumya495 <soumyabanerjee783@gmail.com>
2023-11-27 13:51:50 +0530

    server setup + db connection

---
+++ server/index.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import connect from "./config/database.js";
+
+const app = express();
+
+// Loading environment variables from .env file
+dotenv.config();
+
+// Setting up port number
+const PORT = process.env.PORT || 8000;
+
+// Setting up allowed origins
+// const allowedOrigins = []
+// if(process.env.NODE_ENV) {
+//   allowedOrigins.push(process.env.CLIENT_URL)
+// } else {
+//   allowedOrigins.push("http://localhost:5173")
+// }
+
+// Middlewares
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.set("trust proxy",1);
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+    safeFileNames: true,
+  })
+);
+
+// Connecting to database
+connect();
+
+app.use('*', (req, res, next) => {
+  //print details of incoming request
+  console.log(`Incoming request: ${req.method} ${req.originalUrl}`);
+  //move to next middleware
+  next();
+})
+
+// Test Route
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Your server is up and running ...",
+  });
+});
+
+// Listening to the server
+app.listen(PORT, () => {
+  console.log(`App is listening at ${PORT}`);
+});
