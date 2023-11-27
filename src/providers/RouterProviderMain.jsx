8e991a593a09601b578677610efed5c51b0aa139
soumya495 <soumyabanerjee783@gmail.com>
2023-11-27 12:53:35 +0530

    fix build

--- src/providers/RouterProviderMain.jsx
+++ src/providers/RouterProviderMain.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../pages/home";
 import Login from "../pages/login";
 import Signup from "../pages/signup";
-import VerifyOtp from "../pages/VerifyOtp";
+import VerifyOtp from "../pages/verifyotp";
 import Error from "../pages/error";
 
 export default function RouterProviderMain() {
